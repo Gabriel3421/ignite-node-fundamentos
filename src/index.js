1fa9928266f5c25fb85aec4bba94cf1f2100351d
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-08-29 20:00:35 -0300

    add nodemon + construindo um CRUD

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 const express = require("express")
 
 const app = express()
-
-app.get("/", (req, res) => {
-  return res.json({ message: "funcionou" })
+let courses = ["curso 1", "curso 2", "curso 3"]
+app.use(express.json())
+app.get("/courses", (req, res) => {
+  return res.json(courses)
+})
+app.post("/courses", (req, res) => {
+  const newCouse = req.body.course
+  courses.push(newCouse)
+  return res.json(courses)
+})
+app.put("/courses/:index", (req, res) => {
+  const updatedCourse = req.body.course
+  const index = Number(req.params.index)
+  console.log(updatedCourse)
+  console.log(index)
+  courses = courses.map((item, i) => {
+    console.log(index === i)
+    if (index === i) {
+      console.log('aki')
+      return updatedCourse
+    }
+    return item
+  })
+  return res.json(courses)
+})
+app.patch("/courses/:index", (req, res) => {
+  const updatedCourse = req.body.course
+  const index = Number(req.params.index)
+  console.log(updatedCourse)
+  console.log(index)
+  courses = courses.map((item, i) => {
+    console.log(index === i)
+    if (index === i) {
+      console.log('aki')
+      return updatedCourse
+    }
+    return item
+  })
+  return res.json(courses)
+})
+app.delete("/courses/:index", (req, res) => {
+  const index = Number(req.params.index)
+  courses = courses.filter((item, i) => i !== index)
+  return res.json(courses)
 })
 
-app.listen(3333, () => console.log('server running'));
+app.listen(3333, () => console.log('server running '));
